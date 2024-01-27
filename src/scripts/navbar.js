d2ed3b348565c00d2d652458db607862e9fa2a4d
Dwi Prasetiyo <dwiprasetiyo223@gmail.com>
2024-01-27 16:10:45 +0700

    feat: navbar 'clean code script navbar.js'

--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -1,12 +1,10 @@
 function handleButtonNav(event) {
-  // Mengganti ikon tombol navigasi antara menu hamburger dan X
-  event.target.className =
-    event.target.className ===
-    "fa-solid fa-bars sm:hidden text-neutral-100 text-3xl"
-      ? "fa-solid fa-xmark sm:hidden text-neutral-100 text-3xl"
-      : "fa-solid fa-bars sm:hidden text-neutral-100 text-3xl";
+  // Mengganti ikon tombol navigasi antara menu hamburger dan
+  event.target.classList.toggle("fa-xmark");
+  event.target.classList.toggle("fa-bars");
 
   // Mengalihkan antara kelas "hidden" dan "block" pada elemen dengan ID "navMobile"
+  const navMobile = document.getElementById("navMobile");
   navMobile.classList.toggle("hidden");
   navMobile.classList.toggle("block");
 }
